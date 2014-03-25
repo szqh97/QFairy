f7de372a9cecfa6f20ac4dcbafb4ff2c356c98c8
szqh97 <szqh97@163.com>
2014-03-25 15:55:10 +0800

    update db.sql for sqlite3 qvod_task

--- db.sql
+++ db.sql
@@ -1,15 +1,11 @@
-drop table if exists qvod_tasks;
-create table qvod_tasks
-(
-    id              int(10) unsigned  not null AUTO_INCREMENT,
-    qvod_url        varchar(1024) not null,
-    hash_code       varchar(128) not null,
-    created_at      timestamp not null default '0000-00-00 00:00:00',
-    updated_at      timestamp not null default '0000-00-00 00:00:00',
-    download_url    varchar(1024),
-    filename        varchar(1024),
-    status          enum('initialized', 'error', 'processing', 'succeed') not null default 'initialized',
-    primary key (id),
-    unique key hash_id(hash_code)
-)
-ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE qvod_task(
+       "id" integer primary key autoincrement,
+       "qvod_url" text not null,
+       "hash_code" varchar(256) not null unique,
+       "created_at" timestamp not null default "0000-00-00 00:00:00", 
+       "updated_at" timestamp not null default current_timestamp,
+       "download_url" varchar(512), 
+       "filename" text,
+       "status" varchar(100) default "initialized",
+       "downloader_pid" interger default -1
+);
